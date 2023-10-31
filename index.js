809f69f46f26414370b8608a9bb1c8fda70b810c
Marina Sheigets <marinasejgec34@gmail.com>
2023-11-01 00:02:29 +0200

    fix: cors origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const aWss = WSServer.getWss();
 
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
+		origin: ['https://paint-online-app.vercel.app'],
 		methods: ['POST', 'GET'],
 		credentials: true,
 	})
